fa781d88069a4472e1ac5d32840151c2fc2b601e
Ciarm-nik <nikcia@libero.it>
2021-04-07 16:08:16 +0200

    Print

--- myScript.js
+++ myScript.js
@@ -3,3 +3,5 @@ var nome = prompt("Qual è il tuo nome?");
 var cognome = prompt("Qual è il tuo cognome?")
 
 var colore = prompt("Qual è il tuo colore preferito?")
+
+document.getElementById("my_name").innerHTML = "nome + cognome + colore + 21"
