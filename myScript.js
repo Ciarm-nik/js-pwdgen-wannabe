b632ec52f22bb2f42177a0ed8c4c34a7b2666a57
Ciarm-nik <nikcia@libero.it>
2021-04-07 16:14:14 +0200

    Esercizio concluso

--- myScript.js
+++ myScript.js
@@ -4,4 +4,4 @@ var cognome = prompt("Qual è il tuo cognome?")
 
 var colore = prompt("Qual è il tuo colore preferito?")
 
-document.getElementById("my_name").innerHTML = "nome + cognome + colore + 21"
+document.getElementById("my_name").innerHTML = nome + cognome + colore + "21";
